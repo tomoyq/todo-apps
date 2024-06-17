00e55da2e743ff041f5cbc4fa36b4d7020e9542e
tomoya <arata5.18830@gmail.com>
2024-06-17 18:32:10 +0900

    create_update_page

--- todo/urls.py
+++ todo/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "todo"
 urlpatterns = [
     path("", views.IndexViews.as_view(), name = "index"),
+    path("test/", views.RegistViews.as_view(), name = "regist"),
+    path("update/<int:pk>/", views.UpdateViews.as_view(), name = "reload")
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,11 +1,22 @@
 from typing import Any
 from django.db.models.query import QuerySet
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.urls import reverse_lazy
 
 from .models import Todo
 
 # Create your views here.
 class IndexViews(generic.ListView):
-    template_name = "todo/index.html"
     model = Todo
+    template_name = "todo/index.html"
+
+class RegistViews(generic.DetailView):
+    model = Todo
+    template_name = "todo/regist.html"
+
+class UpdateViews(generic.UpdateView):
+    model = Todo
+    template_name = "todo/update.html"
+    fields = "__all__"
+    success_url = reverse_lazy('todo:index')
